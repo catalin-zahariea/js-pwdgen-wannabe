5db8afbab24b0dee66ae72753b3c818bfa05acd6
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-03-18 16:50:05 +0100

    Basic structure with JS. Next step: add CSS.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var nameUser;
-var userSurname;
-var userColor;
+var nameUser = prompt("Inserisca il suo nome");
+var surnameUser = prompt("Inserisca il suo cognome");
+var colorUser = prompt("Inserisca il suo colore preferito");
 var securityProtocol;
 
 securityProtocol = "21";
 console.log(securityProtocol);
+
+document.getElementById("ultraHighSecurityPassword").innerHTML =
+  nameUser + surnameUser + colorUser + securityProtocol;
